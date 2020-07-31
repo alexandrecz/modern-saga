33263dcbe0a0dd94eee77425f6838c439d4c01f3
Czechowicz <A_Czechowicz@Dell.com>
2020-07-31 18:46:38 -0300

    first commit

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const Nav = () => {
+    
+    return (
+        <nav>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>React Saga</h1>            
+        </nav>
+    )
+}
+
+export default Nav;
